4657eb1c22475bceb79c7f186179f56e6cf4af93
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-15 21:48:38 +0530

    added routes and links

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import { Link } from "react-router-dom";
 import "./ColorBox.css";
 
 class ColorBox extends Component {
@@ -31,9 +32,11 @@ class ColorBox extends Component {
             <div className="box-content">
               <span>{name}</span>
             </div>
-            <span className="see-more">More</span>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <Link to="/" onClick={(e) => e.stopPropagation()}>
+            <span className="see-more">More</span>
+          </Link>
         </div>
       </CopyToClipboard>
     );
